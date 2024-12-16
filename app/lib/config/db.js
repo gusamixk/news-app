886938c3db4c20dbea2484385f5df1641d7ef123
rizkyyunizteresya <132906708+rizkyyunizteresya@users.noreply.github.com>
2024-12-16 13:41:02 +0700

    api

--- app/lib/config/db.js
+++ app/lib/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export const ConnectDB = async () =>{
     await mongoose.connect('mongodb+srv://jurnalight:987654321@cluster0.0wbab.mongodb.net/');
-    console.log("DB Connected")
+    console.log("DB Connected")  
 }
